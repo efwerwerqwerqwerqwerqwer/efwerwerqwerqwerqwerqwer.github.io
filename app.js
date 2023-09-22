279f8c6cbc7c07a0dd5d545b2ac2f82d475fe3ab
efwerwerqwerqwerqwerqwer <85590183+efwerwerqwerqwerqwerqwer@users.noreply.github.com>
2023-09-22 02:02:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById('message-form').addEventListener('submit', async (event)
   const message = messageInput.value;
 
   // Replace this with your Discord webhook URL
-  const webhookUrl = 'https://discord.com/api/webhooks/...';
+  const webhookUrl = 'https://discord.com/api/webhooks/1154673897782902854/sUI5AAm44Pk3-GzEv97t_fXcD6IRj2-JJ72Mj3Q5yzNXpoGjiLfm14fje1xJPG57mLC6';
 
   try {
     await fetch(webhookUrl, {
