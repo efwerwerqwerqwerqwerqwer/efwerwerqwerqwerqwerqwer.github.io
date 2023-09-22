d6d9b3425ed328f9d2ae31085cddc9fc3251e763
efwerwerqwerqwerqwerqwer <85590183+efwerwerqwerqwerqwerqwer@users.noreply.github.com>
2023-09-22 03:29:32 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
 self.addEventListener('install', (event) => {
-  console.log('Service worker installed');
+  event.waitUntil(
+    caches.open('multi-tab-pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Fetching:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
