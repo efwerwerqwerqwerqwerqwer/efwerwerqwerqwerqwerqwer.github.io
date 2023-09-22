02f9510a7a4c247f76969d85ed44587b3aab0ac3
efwerwerqwerqwerqwerqwer <85590183+efwerwerqwerqwerqwerqwer@users.noreply.github.com>
2023-09-22 02:58:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').then(
+      (registration) => {
+        console.log('ServiceWorker registration successful with scope:', registration.scope);
+      },
+      (err) => {
+        console.log('ServiceWorker registration failed:', err);
+      }
+    );
+  });
+}
+
 document.getElementById('collectData').addEventListener('click', async () => {
   const userAgent = navigator.userAgent;
   const language = navigator.language;
